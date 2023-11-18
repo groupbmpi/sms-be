e6b9fa810cb8c47b2946439de4d0f4a87123c864
dwikalam <dwiikalam@gmail.com>
2023-11-18 14:20:56 +0700

    feat: custom class exceptions

---
+++ exceptions/BadRequestException.ts
@@ -0,0 +1,12 @@
+class BadRequestException extends HttpException {
+    public static readonly MESSAGE = 'Bad Request';
+
+    public static readonly STATUS_CODE = 400;
+
+    constructor(message: string = BadRequestException.MESSAGE) {
+        super(
+            BadRequestException.STATUS_CODE, 
+            message
+        );
+    }
+}
