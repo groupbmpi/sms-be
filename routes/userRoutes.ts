2e33fd0a5eddee017beb3f62b3de79074f139ab5
primahafiz <primahafiz@gmail.com>
2023-12-05 07:52:09 +0700

    chore: change reigister user routes

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -3,7 +3,7 @@ import { UserController } from "@controllers"
 
 class UserRoutes extends BaseRoutes {
    public setRoutes(): void {
-      this.routes.post("/", UserController.registerUser);
+      this.routes.post("/register/", UserController.registerUser);
 
       this.routes.put("/verify/", UserController.verifyUser);
       this.routes.get("/verify/", UserController.getUnverifiedUser);
