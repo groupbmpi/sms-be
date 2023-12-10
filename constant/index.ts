9ad52d787aa35ceb1f1e8bd0e58b267cf8104a72
Vincent Atmadja <vincentpatmadja@gmail.com>
2023-12-10 11:54:09 +0700

    feat: add constants for user access

--- constant/index.ts
+++ constant/index.ts
@@ -1,3 +1,17 @@
 // user
 export const OTP_LENGTH = 6;
 export const SALT_ROUND = 10;
+
+//Access control Action
+export const READ = "read";
+export const WRITE = "write";
+export const UPDATE = "update";
+export const DELETE = "delete";
+
+//Access control Object
+export const USER = "user";
+export const LEMBAGA = "lembaga";
+export const BERITA = "berita";
+export const LAPORAN_KEGIATAN = "laporankegiatan";
+export const LAPORAN_MASALAH = "laporanmasalah";
+
