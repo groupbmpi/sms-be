d90ba2334fa00101f8b881a3e499ae8e24079ce5
dwikalam <dwiikalam@gmail.com>
2023-12-15 05:21:35 +0700

    feat: new news flow
    TODO: auth sync

--- routes/newsRoutes.ts
+++ routes/newsRoutes.ts
@@ -5,6 +5,7 @@ import { BaseRoutes } from "@routes";
 class NewsRoutes extends BaseRoutes {
     public setRoutes(): void {
         this.routes.get('/', NewsController.getAllNews);
+        this.routes.get('/optimum-dates', NewsController.getNewsOptimumDates);
         this.routes.get('/:newsId', [paramNewsRequestMiddleware], NewsController.getNewsById);
         this.routes.post('/', [createNewsRequestMiddleware], NewsController.createNews);
         this.routes.put('/:newsId', [updateNewsRequestMiddleware], NewsController.updateNews);
